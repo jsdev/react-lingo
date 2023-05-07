dc66316c2d428c940915d61073a6d5b4c8bbe3fa
Anthony Delorie <AnthonyDeloire@mac-daddy.lan>
2023-05-07 02:49:13 -0400

    spacing

--- src/components/modals/HintModal.tsx
+++ src/components/modals/HintModal.tsx
@@ -28,31 +28,31 @@ export const HintModal = ({ isOpen, handleClose }: Props) => {
           </tr>
         </thead>
         <tbody>
-          <tr><td className="text-right">12%</td><td>AR</td></tr>
-          <tr><td className="text-right">26%</td><td>BA</td></tr>
-          <tr><td className="text-right">26%</td><td>CO</td></tr>
-          <tr><td className="text-right">32%</td><td>DE</td></tr>
-          <tr><td className="text-right">27%</td><td>EN</td></tr>
-          <tr><td className="text-right">21%</td><td>FO</td></tr>
-          <tr><td className="text-right">25%</td><td>GA</td></tr>
-          <tr><td className="text-right">29%</td><td>HO</td></tr>
-          <tr><td className="text-right">54%</td><td>IN</td></tr>
-          <tr><td className="text-right">29%</td><td>JU</td></tr>
-          <tr><td className="text-right">32%</td><td>KI</td></tr>
-          <tr><td className="text-right">31%</td><td>LA</td></tr>
-          <tr><td className="text-right">29%</td><td>MA</td></tr>
-          <tr><td className="text-right">32%</td><td>NE</td></tr>
-          <tr><td className="text-right">26%</td><td>OU</td></tr>
-          <tr><td className="text-right">24%</td><td>PA</td></tr>
-          <tr><td className="text-right">100%</td><td>QU</td></tr>
-          <tr><td className="text-right">55%</td><td>RE</td></tr>
-          <tr><td className="text-right">17%</td><td>ST</td></tr>
-          <tr><td className="text-right">17%</td><td>TO</td></tr>
-          <tr><td className="text-right">58%</td><td>UN</td></tr>
-          <tr><td className="text-right">33%</td><td>VI</td></tr>
-          <tr><td className="text-right">34%</td><td>WI</td></tr>
-          <tr><td className="text-right">100%</td><td>YE</td></tr>
-          <tr><td className="text-right">33%</td><td>ZA</td></tr>
+          <tr><td className="text-right pr-3">12%</td><td>AR</td></tr>
+          <tr><td className="text-right pr-3">26%</td><td>BA</td></tr>
+          <tr><td className="text-right pr-3">26%</td><td>CO</td></tr>
+          <tr><td className="text-right pr-3">32%</td><td>DE</td></tr>
+          <tr><td className="text-right pr-3">27%</td><td>EN</td></tr>
+          <tr><td className="text-right pr-3">21%</td><td>FO</td></tr>
+          <tr><td className="text-right pr-3">25%</td><td>GA</td></tr>
+          <tr><td className="text-right pr-3">29%</td><td>HO</td></tr>
+          <tr><td className="text-right pr-3">54%</td><td>IN</td></tr>
+          <tr><td className="text-right pr-3">29%</td><td>JU</td></tr>
+          <tr><td className="text-right pr-3">32%</td><td>KI</td></tr>
+          <tr><td className="text-right pr-3">31%</td><td>LA</td></tr>
+          <tr><td className="text-right pr-3">29%</td><td>MA</td></tr>
+          <tr><td className="text-right pr-3">32%</td><td>NE</td></tr>
+          <tr><td className="text-right pr-3">26%</td><td>OU</td></tr>
+          <tr><td className="text-right pr-3">24%</td><td>PA</td></tr>
+          <tr><td className="text-right pr-3">100%</td><td>QU</td></tr>
+          <tr><td className="text-right pr-3">55%</td><td>RE</td></tr>
+          <tr><td className="text-right pr-3">17%</td><td>ST</td></tr>
+          <tr><td className="text-right pr-3">17%</td><td>TO</td></tr>
+          <tr><td className="text-right pr-3">58%</td><td>UN</td></tr>
+          <tr><td className="text-right pr-3">33%</td><td>VI</td></tr>
+          <tr><td className="text-right pr-3">34%</td><td>WI</td></tr>
+          <tr><td className="text-right pr-3">100%</td><td>YE</td></tr>
+          <tr><td className="text-right pr-3">33%</td><td>ZA</td></tr>
         </tbody>
       </table>
     </BaseModal>
