2062552b1ced2eb1867eb00e8bd4373811422b3c
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2023-08-11 21:00:00 +0000

    Update SettingsToggle.tsx accessibility
    swapped buttons for checkboxes to convey state

--- src/components/modals/SettingsToggle.tsx
+++ src/components/modals/SettingsToggle.tsx
@@ -1,11 +1,11 @@
-import classnames from 'classnames';
+import classnames from 'classnames'
 
 type Props = {
   settingName: string
   flag: boolean
-  handleFlag: (bool: boolean) => void
+  handleFlag: Function
   description?: string
-};
+}
 
 export const SettingsToggle = ({
   settingName,
@@ -14,33 +14,39 @@ export const SettingsToggle = ({
   description,
 }: Props) => {
   const toggleHolder = classnames(
-    'w-14 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out dark:bg-black-800',
+    'w-14 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out',
     {
-      'bg-green-400 dark:bg-green-800': flag,
-    },
-  );
+      'bg-green-400': flag,
+    }
+  )
   const toggleButton = classnames(
-    'bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out dark:bg-gray-800',
+    'bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out',
     {
       'translate-x-6': flag,
-    },
-  );
+    }
+  )
+
+  const camelCase = (str: string): string =>
+    str.replace(/ (.)/g, (match) => match.toUpperCase()).replace(/ /g, '')
 
   return (
     <>
-      <div className="flex justify-between gap-4 py-3 items-center">
-        <div className="text-left text-gray-500 dark:text-gray-300">
-          <p id={settingName} className="leading-none">{settingName}</p>
-          {description && (
-            <p className="mt-1 text-xs text-gray-500 dark:text-gray-300">
-              {description}
-            </p>
-          )}
+      <label className="switch-card items-center py-3 text-left dark:text-gray-100">
+        <div>
+          {settingName}
+          <p className="mt-1 text-xs text-gray-500 dark:text-gray-300">
+            {description}
+          </p>
         </div>
-        <button aria-labelledby={settingName} className={toggleHolder} onClick={() => handleFlag(!flag)}>
-          <div className={toggleButton} />
-        </button>
-      </div>
+        <div className={toggleHolder}>
+          <input
+            data-id={camelCase(settingName)}
+            onClick={() => handleFlag(!flag)}
+            type="checkbox"
+            className={toggleButton}
+          />
+        </div>
+      </label>
     </>
-  );
-};
+  )
+}
